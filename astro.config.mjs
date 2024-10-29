67a3eb056c5ad6e8cef8651c8be9b6f0eb5dbea0
woogie <woogisfree@gmail.com>
2024-10-29 16:00:21 +0900

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://woogisfree.github.io/',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight',
+				  },
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
